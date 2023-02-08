4a1a3b03a2ab5f7b4ea3f4bd592975ae007452e6
lilygomes <44775170+lilygomes@users.noreply.github.com>
2023-02-08 00:32:46 -0500

    Added icons that spawn windows.

--- Assets/Scripts/CreateWindow.cs
+++ Assets/Scripts/CreateWindow.cs
@@ -1,18 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateWindow : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Button _button;
+    
+    // Program to launch
+    public GameObject program;
+    // UI Canvas to put window on
+    public Canvas canvas;
+    
+    void Awake() {
+        _button = GetComponent<Button>();
+        _button.onClick.AddListener(OnMouseDown);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
-        
+        Instantiate(program, canvas.transform);
     }
 }
